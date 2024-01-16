287ec2543f73e3e3bf87cdd1e5132bd807ea1059
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-16 12:34:50 +0200

    Create a page (via a function in the views file), e.g. autoservice/cars, which would display all the cars registered in the service. Clicking on the car link would display detailed information about the car (owner, car model, registration number, VIN code) Create a page (via a class in the views file), e.g. autoservice/orders, which would display all service orders. Clicking on the order link would display the order details. Order line information would also be visible here. ADDITIONAL: Make the order amount appear next to the order line. And with the order - the total amount of the order.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -3,7 +3,14 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from .models import *
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vehicles/', views.vehicles, name='vehicles'),
+    path('vehicles/<int:vehicle_id>', views.vehicle, name='vehicle'),
+    path('services/', views.services, name='services'),
+    path('services/<int:service_id>', views.service, name='service'),
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('orders/<int:pk>', views.OrderDetailView.as_view(), name='order_detail'),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 # Create your views here.
 from django.http import HttpResponse
@@ -19,3 +21,44 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+def vehicles(request):
+    vehicles = Vehicle.objects.all()
+    context = {
+        'vehicles': vehicles
+    }
+    return render(request, 'vehicles.html', context=context)
+
+
+def vehicle(request, vehicle_id):
+    single_vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+    return render(request, 'vehicle.html', {'vehicle': single_vehicle,})
+
+
+def services(request):
+    services = Service.objects.all()
+    context = {
+        'services': services
+    }
+    return render(request, 'services.html', context=context)
+
+
+def service(request, service_id):
+    single_service = get_object_or_404(Service, pk=service_id)
+    return render(request, 'service.html', {'service': single_service,})
+
+# Sukurti puslapį (per klasę views faile),
+# pvz. autoservice/uzsakymai, kuriame būtų atvaizduoti visi serviso užsakymai.
+
+
+class OrderListView(generic.ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+
