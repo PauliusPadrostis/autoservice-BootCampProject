7704c2b61a296db7fd3e4dd8f3a2ac49c6b06a83
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-16 08:24:00 +0200

    Make the customer, car, license plate and VIN number columns visible in the car list

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -13,10 +13,13 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('date', 'vehicle', 'total')
     inlines = [OrderInLine]
 
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('client', 'model', 'plate', 'vin')
+
 
 # Register your models here.
 admin.site.register(Model)
-admin.site.register(Vehicle)
+admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
 admin.site.register(Service)
