da109ada6c0313e4617385646eb407eed3f54ef7
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-16 08:19:13 +0200

    Make the car and date columns visible in the order list

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+
+# Class Inlines
+
+# AdminClasses
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('date', 'vehicle', 'total')
+
+
 # Register your models here.
 admin.site.register(Model)
 admin.site.register(Vehicle)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
 admin.site.register(Service)
